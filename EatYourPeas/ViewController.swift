457a20d4224cc30db2527f0b8d42889ead7a8b4c
Chang Sophia <you@example.com>
2018-10-03 16:38:42 +0800

    Initial Commit

---
+++ EatYourPeas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EatYourPeas
+//
+//  Created by Chang Sophia on 2018/10/3.
+//  Copyright © 2018年 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
